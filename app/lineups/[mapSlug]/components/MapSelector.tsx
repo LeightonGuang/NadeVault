2e0d0099a4722bc2f92a3cd836b136e609211db0
LeightonGuang <leighton.guang@icloud.com>
2026-04-07 10:15:11 +0100

    feat: new map selector component

---
+++ app/lineups/[mapSlug]/components/MapSelector.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import Image from "next/image";
+import getMaps from "@/assets/getMaps";
+import { twMerge } from "tailwind-merge";
+
+import { MapSlug } from "@/types/Map";
+
+const MapSelector = ({
+  className,
+  mapSlug,
+}: {
+  className?: string;
+  mapSlug: MapSlug;
+}) => {
+  const maps = getMaps();
+
+  return (
+    <div className={twMerge("flex h-8 w-full items-center px-2", className)}>
+      {maps.map((map) => (
+        <Link
+          key={map.name}
+          href={`/lineups/${map.slug}/all`}
+          className={twMerge(
+            "hover:bg-primary flex h-full items-center gap-1 px-2 whitespace-nowrap",
+            mapSlug === map.slug ? "bg-primary" : "",
+          )}
+        >
+          <Image
+            src={map.emblemUrl}
+            alt={map.name}
+            width={20}
+            height={20}
+            className="object-contain"
+          />
+          <span className="text-sm font-bold text-white">{map.name}</span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default MapSelector;
