1a1f434f5e3febc2b82b275e39f7720e1eaec3bf
LeightonGuang <leighton.guang@icloud.com>
2026-04-07 10:14:46 +0100

    style: new navbar design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,49 +2,56 @@ import Link from "next/link";
 
 const navLinks = [
   {
-    href: "/",
+    href: "/home",
     label: "Home",
+    enabled: true,
   },
   {
-    href: "/lineups",
+    href: "/lineups/dust2",
     label: "Lineups",
+    enabled: true,
   },
   {
-    href: "/projects",
-    label: "Projects",
+    href: "/cheater-checker",
+    label: "Cheater Checker",
+    enabled: false,
   },
   {
     href: "/contact",
     label: "Contact",
+    enabled: true,
   },
 ];
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 right-0 left-0 z-50 border-b border-gray-200 bg-white/80 backdrop-blur-md dark:border-white/10 dark:bg-black/80">
+    <nav className="fixed top-0 right-0 left-0 z-50 h-(--nav-height) border-b border-white/5 bg-black/50 backdrop-blur-md">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
+        <div className="flex h-(--nav-height) max-w-(--max-width) items-center justify-between">
           <div className="flex shrink-0 items-center">
             <Link
-              href="/"
-              className="bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-2xl font-bold text-transparent uppercase"
+              href="/home"
+              className="font-[Hoover] text-2xl font-bold tracking-tight uppercase transition-all duration-200 hover:scale-102"
             >
-              Vaulted
+              <h1>
+                <span className="text-primary leading-none">Nade</span>
+                <span className="leading-none text-white"> Vault</span>
+              </h1>
             </Link>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
             {navLinks.map((link) => (
               <Link
                 key={link.label}
-                href={link.href}
-                className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-900 uppercase transition-colors hover:border-indigo-500 dark:text-gray-100"
+                href={link.enabled ? link.href : "#"}
+                className={`inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white uppercase transition-colors ${link.enabled ? "text-foreground hover:border-primary hover:text-primary" : "text-foreground/40 hover:cursor-default"} `}
               >
                 {link.label}
               </Link>
             ))}
           </div>
           <div className="flex items-center space-x-4">
-            <button className="transform rounded-full bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-lg shadow-indigo-500/25 transition-all hover:scale-105 hover:bg-indigo-700 active:scale-95">
+            <button className="bg-primary transform italic cursor-pointer px-4 py-2 text-sm font-medium text-white shadow-lg shadow-red-900/40 transition-all [clip-path:var(--button-clip-path)] hover:scale-105 hover:bg-red-700 active:scale-95">
               Get Started
             </button>
           </div>
