797880d0c117b97cfc5e690d2be771194bb5f00a
LeightonGuang <leighton.guang@icloud.com>
2026-04-18 03:25:16 +0100

    feat: add enabled property to support map availability toggling

--- utils/getMaps.ts
+++ utils/getMaps.ts
@@ -1,16 +1,66 @@
 import { MapSlug } from "@/types/Map";
 
 const getMaps = () => {
-  const maps: { name: string; slug: MapSlug; emblemUrl: string }[] = [
-    { name: "Ancient", slug: "ancient", emblemUrl: "/emblem/ancient.png" },
-    { name: "Anubis", slug: "anubis", emblemUrl: "/emblem/anubis.png" },
-    { name: "Dust 2", slug: "dust2", emblemUrl: "/emblem/dust2.png" },
-    { name: "Inferno", slug: "inferno", emblemUrl: "/emblem/inferno.png" },
-    { name: "Mirage", slug: "mirage", emblemUrl: "/emblem/mirage.png" },
-    { name: "Nuke", slug: "nuke", emblemUrl: "/emblem/nuke.png" },
-    { name: "Overpass", slug: "overpass", emblemUrl: "/emblem/overpass.png" },
-    { name: "Train", slug: "train", emblemUrl: "/emblem/train.png" },
-    { name: "Vertigo", slug: "vertigo", emblemUrl: "/emblem/vertigo.png" },
+  const maps: {
+    name: string;
+    slug: MapSlug;
+    emblemUrl: string;
+    enabled: boolean;
+  }[] = [
+    {
+      name: "Ancient",
+      slug: "ancient",
+      emblemUrl: "/emblem/ancient.png",
+      enabled: true,
+    },
+    {
+      name: "Anubis",
+      slug: "anubis",
+      emblemUrl: "/emblem/anubis.png",
+      enabled: true,
+    },
+    {
+      name: "Dust 2",
+      slug: "dust2",
+      emblemUrl: "/emblem/dust2.png",
+      enabled: true,
+    },
+    {
+      name: "Inferno",
+      slug: "inferno",
+      emblemUrl: "/emblem/inferno.png",
+      enabled: true,
+    },
+    {
+      name: "Mirage",
+      slug: "mirage",
+      emblemUrl: "/emblem/mirage.png",
+      enabled: true,
+    },
+    {
+      name: "Nuke",
+      slug: "nuke",
+      emblemUrl: "/emblem/nuke.png",
+      enabled: false,
+    },
+    {
+      name: "Overpass",
+      slug: "overpass",
+      emblemUrl: "/emblem/overpass.png",
+      enabled: true,
+    },
+    {
+      name: "Train",
+      slug: "train",
+      emblemUrl: "/emblem/train.png",
+      enabled: false,
+    },
+    {
+      name: "Vertigo",
+      slug: "vertigo",
+      emblemUrl: "/emblem/vertigo.png",
+      enabled: false,
+    },
   ];
 
   return maps;
