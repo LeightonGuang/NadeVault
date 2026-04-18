7fc507f5af7d611289f4edb5e38fdb814bc72e46
LeightonGuang <leighton.guang@icloud.com>
2026-04-18 18:48:40 +0100

    feat: new pill design

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -1,19 +1,26 @@
+import { twMerge } from "tailwind-merge";
+
 const Pill = ({
   children,
   variant,
   style = "default",
 }: {
   children: React.ReactNode;
-  variant: "active" | "inactive";
+  variant: "active" | "inactive" | "dark-red";
   style?: "default" | "dot";
 }) => {
+  const pillStyle = {
+    active: "bg-primary text-white",
+    inactive: "bg-zinc-800 text-zinc-400",
+    "dark-red": "border-primary/20 bg-primary/5 text-primary",
+  };
+
   return style === "default" ? (
     <span
-      className={`rounded-full border border-white/5 px-3 py-1 text-[0.625rem] font-black uppercase transition-colors ${
-        variant === "active"
-          ? "bg-primary text-white"
-          : "bg-zinc-800 text-zinc-400"
-      }`}
+      className={twMerge(
+        `rounded-full border border-white/5 px-3 py-1 text-xs font-bold tracking-widest uppercase transition-colors`,
+        pillStyle[variant],
+      )}
     >
       {children}
     </span>
