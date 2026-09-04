63f2fa34092ac8d4338f5eb72b7fb6db49a6e0da
LeightonGuang <leighton.guang@icloud.com>
2026-09-04 04:28:22 +0100

    feat: new mirage lineups

---
+++ lib/api/lineups/mirage.ts
@@ -0,0 +1,78 @@
+import { Lineup } from "@/types/Lineup";
+
+export const mirageLineups: Lineup[] = [
+  {
+    id: 1,
+    name: "Window smoke from T spawn",
+    mapSlug: "mirage",
+    type: "smoke",
+    description: "",
+    throwType: "jump throw",
+    precision: 2,
+    duration: 5,
+    team: "T",
+    radarIndex: 0,
+    points: [
+      {
+        x: 667.2867994925751,
+        y: 941.2104502980707,
+      },
+      {
+        x: 564.6144568959256,
+        y: 409.80795766242863,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Connector smoke from T spawn",
+    mapSlug: "mirage",
+    type: "smoke",
+    description: "",
+    throwType: "jump throw",
+    precision: 2,
+    duration: 5,
+    team: "T",
+    radarIndex: 0,
+    points: [
+      {
+        x: 690.2285861610542,
+        y: 934.9854166288924,
+      },
+      {
+        x: 516.9500834984319,
+        y: 500.1885957665252,
+      },
+      {
+        x: 505.8480780922894,
+        y: 528.1118214850047,
+      },
+      {
+        x: 501.81098521732855,
+        y: 523.0654553913037,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Kitchen window smoke from back alley",
+    mapSlug: "mirage",
+    type: "smoke",
+    description: "",
+    throwType: "jump throw",
+    precision: 2,
+    duration: 5,
+    team: "T",
+    radarIndex: 0,
+    points: [
+      {
+        x: 862.6047494670089,
+        y: 614.6795066389459,
+      },
+      {
+        x: 628.3820290286806,
+        y: 254.40970591696757,
+      },
+    ],
+  },
+];
